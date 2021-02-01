9bb827a092272003051d08af94994b7cb0db7d40
lenakernytska <yaapril222@gmail.com>
2021-02-01 17:31:53 +0200

    Updates LocalStorage memory

--- src/js/localStorage.js
+++ src/js/localStorage.js
@@ -3,38 +3,26 @@
 
 
 queue (element){
-
-    document.body.addEventListener( 'click', function ( event ) {
-
-        if(event.toElement.className !== 'modal-btns-add-to-queue' ){
+document.body.addEventListener( 'click', function ( event ) {
+if(event.toElement.className !== 'modal-btns-add-to-queue' ){
             return
         }
         else {
-    console.log('URA');
-    console.log(element);
     saveQueue(element);
     }
-    
-      });
-
-    },
+     });
+},
 
     watched (element){
-
-      document.body.addEventListener( 'click', function ( event ) {
-  
-          if(event.toElement.className !== 'modal-btns-left'){
+document.body.addEventListener( 'click', function ( event ) {
+   if(event.toElement.className !== 'modal-btns-left'){
               return
           }
           else {
-      console.log('watched');
-      console.log(element);
-      saveQueue(element);
+      saveToWatchedList(element);
       }
-      
-        });
-  
-      },
+      });
+  },
 }
 
  function saveQueue(id){
@@ -42,19 +30,24 @@ let newId = id;
 if (localStorage.getItem('queue') === null){
     localStorage.setItem('queue', '[]');
 }
+let arr = JSON.parse(localStorage.getItem('queue'));
+const i = arr.indexOf(newId);
+if (i === -1) {
+    arr.push(newId)
+}
+localStorage.setItem('queue', JSON.stringify(arr));
+}
+
+
+function saveToWatchedList(id) {
+   let newId = id;
 if (localStorage.getItem('watched') === null){
   localStorage.setItem('watched', '[]');
 }
-let arr = JSON.parse(localStorage.getItem('queue'));
 let arr = JSON.parse(localStorage.getItem('watched'));
 const i = arr.indexOf(newId);
 if (i === -1) {
     arr.push(newId)
 }
-
-localStorage.setItem('queue', JSON.stringify(arr));
 localStorage.setItem('watched', JSON.stringify(arr));
-}
-
-
-  
+} 
